9542cd8246ad586a94753aadacf473a97e93411a
Aquib Shahbaz <justaquib@Aquibs-MacBook-Air.local>
2026-04-13 14:58:07 +0530

    Add Vercel deployment configuration

--- xmerge_dev/settings.py
+++ xmerge_dev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8mgvmf4zk=4b51$#c#+w1-a*s!ln))8gd0_gd@3&dy+6zy#^1y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- xmerge_dev/wsgi.py
+++ xmerge_dev/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xmerge_dev.settings')
-
 application = get_wsgi_application()
+
+app = application
